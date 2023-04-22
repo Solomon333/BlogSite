8c0fc3b48250ce879f75383a7cb8d212b7c0a750
Salomon Solomon <ssalomon783@gmail.com>
2023-04-23 00:05:15 +0300

    update footer

--- app.js
+++ app.js
@@ -34,6 +34,11 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 })
 
+app.post("/post", function(req, res) {
+  const post = req.body.newPost
+  console.log(post) 
+})
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
