cfa696ea6ee3f26c520e623dff9514da35d500ef
Salomon Solomon <ssalomon783@gmail.com>
2023-04-24 12:17:28 +0300

    render posts

--- app.js
+++ app.js
@@ -11,41 +11,49 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-let posts = [];
-
 const app = express();
-
 app.set("view engine", "ejs");
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", function (req, res) {
-  res.render("home", { homeString: homeStartingContent });
+  res.render("home", {
+     homeString: homeStartingContent,
+     posts: posts
+    });
 });
 
 app.get("/about", function (req, res) {
-  res.render("about", { aboutString: aboutContent });
+  res.render("about", { 
+    aboutString: aboutContent 
+  });
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact", { contact: contactContent });
+  res.render("contact", { 
+    contact: contactContent
+   });
 });
 
-app.get("/compose", function(req, res) {
+app.get("/compose", function (req, res) {
   res.render("compose");
-})
+});
 
-app.post("/compose", function(req, res) {
+app.post("/compose", function (req, res) {
   const post = {
     title: req.body.postTitle,
-    body: req.body.postBody
-  }
-    posts.push(post.body);
-    console.log(posts)
-    res.redirect("/");
-  
-})
+    body: req.body.postBody,
+  };
+  posts.push(post);
+
+  res.redirect("/");
+});
+
+
+
+
 
 app.listen(3000, function () {
   console.log("Server started on port 3000");
