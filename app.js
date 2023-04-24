e328aaf5389f35e8e442106ecc45e1e8ac3406c0
Salomon Solomon <ssalomon783@gmail.com>
2023-04-24 19:37:29 +0300

    render post page

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -51,7 +52,20 @@ app.post("/compose", function (req, res) {
   res.redirect("/");
 });
 
+app.get("/posts/:postTitle", function(req, res) {
+  const reqestedTitle = _.lowerCase(req.params.postTitle);
+ 
+  posts.forEach(function(post) {
+    const storedTitle = _.lowerCase(post.title)
 
+    if(reqestedTitle === storedTitle) {
+      res.render("post", {
+        title: post.title,
+        body: post.body
+      });
+    }
+  });
+  })
 
 
 
